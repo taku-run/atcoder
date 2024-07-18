33cafc519459484a78b006da220457264436d3e9
taku-run <takurun0108@outlook.com>
2024-07-18 18:27:38 +0900

    Add files via upload

---
+++ atcoder/ABC/312/a.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+using ll = long long;
+using P = pair<int,int>;
+
+int main() {
+  string S;
+  cin >> S;
+
+  if (S == "ACE" || S == "BDF" || S == "CEG" || S == "DFA" || S == "EGB" || S == "FAC" || S == "GBD")
+  {
+    cout << "Yes" << endl;
+  }
+  else
+  {
+    cout << "No" << endl;
+  }
+
+  return 0;
+}
